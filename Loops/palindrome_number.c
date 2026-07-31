4b181a32aaa85a0c827fef35e7397b5b3c21e3f3
niharikaketavarapu0106-08 <niharikaketavarapu0106@gmail.com>
2026-07-31 23:19:40 +0530

    Create palindrome_number.c

---
+++ Loops/palindrome_number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, temp, rev = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while(temp != 0) {
+        rev = rev * 10 + temp % 10;
+        temp /= 10;
+    }
+
+    if(rev == n)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
