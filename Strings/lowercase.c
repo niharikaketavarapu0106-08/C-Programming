52438d5800774f9e673afebe75ad60aa3ad0ac4d
niharikaketavarapu0106-08 <niharikaketavarapu0106@gmail.com>
2026-08-01 21:10:36 +0530

    Add lowercase string conversion program

---
+++ Strings/lowercase.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        str[i] = tolower(str[i]);
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
